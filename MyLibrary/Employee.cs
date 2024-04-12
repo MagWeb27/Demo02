08d94591d1f4ce3d901f776227967aec14eb1e08
Miguel Angel Martinez Gomez <miguelmartinezgomez27@gmail.com>
2024-04-12 00:10:23 -0500

    Actualizacion metodo getpaymentdays

--- MyLibrary/Employee.cs
+++ MyLibrary/Employee.cs
@@ -20,6 +20,11 @@ namespace MyLibrary;
         {
             return 12000;
         }
+
+        public int GetPaymentDays()
+        {
+            return 30;
+        }
     }
     
     public class Secretary : Employee
@@ -37,7 +42,10 @@ public class Manager : Employee
 
 public class Developer : Employee
 {
-
+    public new int GetPaymentDays()
+    {
+        return 20;
+    }
 }
 
 
